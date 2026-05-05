e0bad6410cb61242cf88ca05983949c4c4351331
chaithesh <chaithesh121@gmail.com>
2026-05-05 10:12:42 +0530

    git modification

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,30 +1,41 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 
-exports.createUser = async (req, res)=>{
-    try{
-        const {name, email,password} = req.body;
+exports.createUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
 
-        console.log('Received user data:', {name, email, password});
+        const existingUser = await User.findOne({ email });
 
-        //hash the password
+        if (existingUser) {
+            return res.status(400).json({
+                message: 'User already exists'
+            });
+        }
+
+        console.log('Received user data:', { name, email, password, role });
+
+        // hash password
         const hashedPassword = await bcrypt.hash(password, 10);
 
-
-        //save to database
-
+        // save to database
         const user = await User.create({
             name,
             email,
-            password:hashedPassword
+            password: hashedPassword,
+            role
         });
 
-        //sending respose
-        res.status(201).json({message: 'User created successfully', user});
-    }
-    catch(error){
-        res.status(500).json({message: 'Error creating user', error: error.message});
-    }
-}  
+        // response
+        res.status(201).json({
+            message: 'User created successfully',
+            user
+        });
 
-
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error creating user',
+            error: error.message
+        });
+    }
+};
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -16,6 +16,11 @@ const UserSchema = new mongoose.Schema(
         password:{
             type : String,
             required : true
+        },
+        role:{
+            type: String,
+            enum: ['user', 'driver'],
+            default: 'user'
         }
     },{
         timestamps : true
